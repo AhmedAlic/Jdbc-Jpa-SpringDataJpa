04bab9d349c4c4e8c9ad4103a074faa08b87617f
aalic <Ahmed.Alic@infobip.com>
2022-02-18 12:35:48 +0100

    Removed import

--- src/main/java/com/database/springdatajpa/service/EmployeeServiceImpl.java
+++ src/main/java/com/database/springdatajpa/service/EmployeeServiceImpl.java
@@ -5,7 +5,6 @@ import com.database.springdatajpa.repository.EmployeeRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
