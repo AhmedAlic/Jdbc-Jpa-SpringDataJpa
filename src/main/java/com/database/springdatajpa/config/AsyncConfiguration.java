c98a124c1974c317a801ba7a8e0fcb8bd4430591
aalic <Ahmed.Alic@infobip.com>
2022-02-21 09:02:48 +0100

    Added async executor

---
+++ src/main/java/com/database/springdatajpa/config/AsyncConfiguration.java
@@ -0,0 +1,27 @@
+package com.database.springdatajpa.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Slf4j
+@Configuration
+@EnableAsync
+public class AsyncConfiguration {
+
+    @Bean
+    public Executor taskExecutor() {
+        log.info("Creating Async task executor...");
+        var executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(2);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("EmployeeWorker-");
+        executor.initialize();
+        return executor;
+    }
+}
